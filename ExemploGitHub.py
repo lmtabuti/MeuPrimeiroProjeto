b114e80b5b24709223bfbe4ad3d12c27dfb95527
lmtabuti <profa.lucymari@gmail.com>
2024-05-26 23:12:25 -0300

    Add files via upload

---
+++ ExemploGitHub.py
@@ -0,0 +1,15 @@
+# Recebe os dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcula as operações
+soma = num1 + num2
+diferenca = num1 - num2
+produto = num1 * num2
+quociente = num1 / num2
+
+# Exibe os resultados em linhas separadas
+print(f"Soma: {soma}")
+print(f"Diferença: {diferenca}")
+print(f"Produto: {produto}")
+print(f"Quociente: {quociente}")
